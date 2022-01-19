94bbb76759c6afc8d91e11303ae203eadde2b876
taiconley <taiconley@gmail.com>
2022-01-19 15:41:06 -0800

    update app

---
+++ app/app.py
@@ -0,0 +1,76 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+from tensorflow import keras
+import xgboost as xgb
+import autokeras as ak
+
+
+app = Flask(__name__)
+
+
+bst = xgb.Booster()  # init model
+bst.load_model('xgb_model.json')  
+
+
+@app.route('/')
+def home():
+    #return render_template('index.html')
+    return "hello"
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    #int_features = [int(x) for x in request.form.values()]
+    #int_features = [float(x) for x in request.form.values()]
+    features = [x for x in request.form.values()]
+    #int_features = [int(x) for x in request.form.values()]
+    # features_list = []
+    # for i in features:
+    #     if i == ',':
+    #         pass
+    #     else:
+    #         features_list.append(i)
+
+    # bst = xgb.Booster()  # init model
+    # bst.load_model('xgb_model.json')  
+    # dtest = xgb.DMatrix(features[0:1])
+    # prediction = bst.predict(dtest)
+    # output = prediction[0]
+
+    
+    # output = features_list
+    # # output = round(prediction[0], 2)
+
+    # return render_template('index.html', prediction_text='Sales should be $ {}'.format(str(output)))
+    return "hello2"
+    
+
+@app.route('/results',methods=['POST'])
+def results():
+    data = request.get_json(force=True)
+    dtest = xgb.DMatrix(data)
+    prediction = bst.predict(dtest)
+    output = prediction[0]
+    return str(output)
+
+# BELOW WORKS
+    # data = request.get_json(force=True)
+
+    # keras_model = keras.models.load_model("model_keras", custom_objects=ak.CUSTOM_OBJECTS)
+    # prediction = keras_model.predict(data)
+    # output = prediction[0][0]
+    # return str(output)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+    
+# def load_keras(filename):
+#     loaded_model = keras.models.load_model(filename, custom_objects=ak.CUSTOM_OBJECTS)
+#     return loaded_model
---
+++ app/request.py
@@ -0,0 +1,9 @@
+import requests
+
+url = 'http://localhost:5000/results'
+
+values = [[-0.16873684, -0.14134896,  0.33981747]]
+r = requests.post(url,json=values)
+
+
+print(r.json())
